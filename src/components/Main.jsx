635894e80a3e2f3049782b804cba336bf6bb906d
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2022-06-13 21:28:37 +0300

    component-seperade

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react";
 import { useLocation } from "../context/Location_Context";
+import Table from "./Table";
 
 function Main() {
   const { daily } = useLocation();
-  const [hover, setHover] = useState(false);
+
   const data = {
+    0: "Pazar",
     1: "Pazartesi",
     2: "Salı",
     3: "Çarşamba",
     4: "Perşembe",
     5: "Cuma",
     6: "Cumartesi",
-    0: "Pazar",
+    7: "Pazar",
+    8: "Pazartesi",
+    9: "Salı",
+    10: "Çarşamba",
+    11: "Perşembe",
+    12: "Cuma",
+    13: "Cumartesi",
+    14: "Pazar",
   };
 
-  const dayName = new Date().getDay();
+  let dayName = new Date().getDay();
   return (
     <div>
       <div className="flex m-10">
@@ -40,19 +48,7 @@ function Main() {
                       : "bg-cyan-400 text-blue-900"
                   }`}
                 >
-                  {dayName + i < 7 ? (
-                    <h2 className="text-3xl text-center">
-                      {data[dayName + i]}
-                    </h2>
-                  ) : i === 7 ? (
-                    <h2 className="text-3xl text-center">{data[dayName]}</h2>
-                  ) : i + dayName > 7 && i + dayName < 14 ? (
-                    <h2 className="text-3xl text-center">
-                      {data[i + dayName - 7]}
-                    </h2>
-                  ) : (
-                    <h2 className="text-3xl text-center">{data[0]}</h2>
-                  )}
+                  <h2 className="text-3xl text-center">{data[dayName + i]}</h2>
 
                   <img
                     className="w-2/3 mx-auto -mt-10"
@@ -86,61 +82,7 @@ function Main() {
         {daily &&
           daily.daily.map(
             (day, i) =>
-              i > 0 && (
-                <div
-                  onClick={() => setHover(!hover)}
-                  key={i}
-                  className={`${
-                    day.clouds > 75
-                      ? "bg-gray-600 text-neutral-200"
-                      : day.clouds > 50
-                      ? "bg-gray-400"
-                      : day.clouds > 35
-                      ? "bg-gray-300"
-                      : day.clouds > 25
-                      ? "bg-cyan-100 text-blue-900"
-                      : day.clouds > 15
-                      ? "bg-cyan-200 text-blue-900"
-                      : day.clouds > 10
-                      ? "bg-cyan-300 text-blue-900"
-                      : "bg-cyan-400 text-blue-900"
-                  } w-36 mb-10 rounded-3xl p-2`}
-                >
-                  <div className="flex justify-evenly">
-                    {dayName + i < 7 ? (
-                      <h2 className="text-md">{data[dayName + i]}</h2>
-                    ) : <h2 className="text-md">{i === 7}</h2> ? (
-                      <h2 className="text-md">{data[dayName]}</h2>
-                    ) : i + dayName > 7 && i + dayName < 14 ? (
-                      <h2 className="text-md">{data[i + dayName - 7]}</h2>
-                    ) : (
-                      <h2 className="text-md">{data[0]}</h2>
-                    )}
-                  </div>
-                  <img
-                    className="mx-auto w-full -mb-5 -mt-5"
-                    src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-                    alt=""
-                  />
-                  <h2 className="text-xl text-center">
-                    {day.weather[0].description}
-                  </h2>
-                  <div className={`${hover === true ? null : "hidden"}`}>
-                    <h4 className="text-center text-md">Hissedilen Sıcaklık</h4>
-                    <div className="flex justify-around ">
-                      <h4 className="text-lg">
-                        Gündüz: {Math.round(day.feels_like.day)}°C
-                      </h4>
-                      <h4 className="text-lg">
-                        Gece: {Math.round(day.feels_like.night)}°C
-                      </h4>
-                    </div>
-
-                    <h4>%{day.clouds} Bulutlu</h4>
-                    <h4>%{day.humidity} Nemli</h4>
-                  </div>
-                </div>
-              )
+              i > 0 && <Table day={day} dayName={dayName} i={i} data={data} />
           )}
       </div>
     </div>
---
+++ src/components/Table.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+
+function Table({ dayName, day, i, data }) {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div className="h-72">
+      <div
+        onClick={() => setHover(!hover)}
+        key={i}
+        className={`${
+          day.clouds > 75
+            ? "bg-gray-600 text-neutral-200"
+            : day.clouds > 50
+            ? "bg-gray-400"
+            : day.clouds > 35
+            ? "bg-gray-300"
+            : day.clouds > 25
+            ? "bg-cyan-100 text-blue-900"
+            : day.clouds > 15
+            ? "bg-cyan-200 text-blue-900"
+            : day.clouds > 10
+            ? "bg-cyan-300 text-blue-900"
+            : "bg-cyan-400 text-blue-900"
+        } w-36 mb-10 rounded-3xl p-2 h-max-[20px]`}
+      >
+        <div className="flex justify-evenly">
+          <h2 className="text-3xl text-center">{data[dayName + i]}</h2>
+        </div>
+        <img
+          className="mx-auto w-full -mb-5 -mt-5"
+          src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+          alt=""
+        />
+        <h2 className="text-xl text-center">{day.weather[0].description}</h2>
+        <div className={`${hover === false ? "hidden" : "text-md"}`}>
+          <h4 className="text-center text-md">Hissedilen Sıcaklık</h4>
+          <div className="flex justify-around ">
+            <h4 className="text-lg">
+              Gündüz: {Math.round(day.feels_like.day)}°C
+            </h4>
+            <h4 className="text-lg">
+              Gece: {Math.round(day.feels_like.night)}°C
+            </h4>
+          </div>
+
+          <h4>%{day.clouds} Bulutlu</h4>
+          <h4>%{day.humidity} Nemli</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
